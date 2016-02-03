26232b9d3d1dba19e843d33cd061eabf604cb8be
thesuss <lullisuss@gmail.com>
2016-02-03 15:30:12 +0000

    immi

--- lib/application.rb
+++ lib/application.rb
@@ -25,7 +25,7 @@ class WorkshopApp < Sinatra::Base
 
   env = ENV['RACK_ENV'] || 'development'
   #  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/workshop_#{env}")
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:postgres@localhost/workshop_#{env}")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:postgres@localhost/immi-certs_#{env}")
     DataMapper::Model.raise_on_save_failure = true
     DataMapper.finalize
     DataMapper.auto_upgrade!
